dc8f55c9d7cd9d2c430a8212aa17a12448ebd320
Yaniv Efraim <yaniv.efraim@gmail.com>
2019-04-05 13:17:13 +0300

    refactor to react hooks

--- src/Registration.js
+++ src/Registration.js
@@ -1,36 +1,24 @@
-import React from 'react';
-export class Registration extends React.Component {
-  state = {
-    player1: '',
-    player2: ''
-  };
-  render() {
-    const { onNewGame } = this.props;
-    return (
-      <div>
-        <input
-          data-testid="player1"
-          onChange={evt =>
-            this.setState({
-              player1: evt.target.value
-            })
-          }
-        />
-        <input
-          data-testid="player2"
-          onChange={evt =>
-            this.setState({
-              player2: evt.target.value
-            })
-          }
-        />
-        <button
-          data-testid="new-game"
-          onClick={() => onNewGame(this.state.player1, this.state.player2)}
-        >
-          New Game
-        </button>
-      </div>
-    );
-  }
+import React, { useState } from 'react';
+export function Registration({ onNewGame }) {
+  const [player1, setPlayer1] = useState('');
+  const [player2, setPlayer2] = useState('');
+
+  return (
+    <div>
+      <input
+        data-testid="player1"
+        onChange={evt => setPlayer1(evt.target.value)}
+      />
+      <input
+        data-testid="player2"
+        onChange={evt => setPlayer2(evt.target.value)}
+      />
+      <button
+        data-testid="new-game"
+        onClick={() => onNewGame(player1, player2)}
+      >
+        New Game
+      </button>
+    </div>
+  );
 }
