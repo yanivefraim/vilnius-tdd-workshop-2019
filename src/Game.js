439e669911eced848a3db4760b5aee502dc9436a
Yaniv Efraim <yaniv.efraim@gmail.com>
2019-04-05 13:06:24 +0300

    second test

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,25 @@
 import React from 'react';
-export const Game = ({ player1, player2 }) => {
+export const Game = ({ player1, player2, board, onCellClicked }) => {
   return (
     <div>
       <h1 data-testid="player1-title">{player1}</h1>
       <h1 data-testid="player2-title">{player2}</h1>
+      <table>
+        <tbody>
+          {board.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, colIndex) => (
+                <td
+                  key={colIndex}
+                  onClick={() => onCellClicked(rowIndex, colIndex)}
+                >
+                  {cell}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
