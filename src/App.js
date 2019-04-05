0b6ab40d2e8b777c64811dcde2ac104ac6036334
Yaniv Efraim <yaniv.efraim@gmail.com>
2019-04-05 13:17:13 +0300

    refactor to react hooks

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Registration } from './Registration';
 import { Game } from './Game';
 
-class App extends Component {
-  state = {
-    player1: '',
-    player2: '',
-    board: [['', '', ''], ['', '', ''], ['', '', '']]
+function App() {
+  const [player1, setPlayer1] = useState('');
+  const [player2, setPlayer2] = useState('');
+  const [board, setBoard] = useState([
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ]);
+
+  const handleNewGame = (player1, player2) => {
+    setPlayer1(player1);
+    setPlayer2(player2);
   };
-  handleNewGame = (player1, player2) => {
-    this.setState({ player1, player2 });
+  const handleCellClicked = (rowIndex, colIndex) => {
+    const _board = board.map(row => [...row]);
+    _board[rowIndex][colIndex] = 'X';
+    setBoard(_board);
   };
-  handleCellClicked = (rowIndex, colIndex) => {
-    const board = this.state.board.map(row => [...row]);
-    board[rowIndex][colIndex] = 'X';
-    this.setState({ board });
-  };
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            data-testid="app-link"
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Registration onNewGame={this.handleNewGame} />
-        <Game
-          player1={this.state.player1}
-          player2={this.state.player2}
-          board={this.state.board}
-          onCellClicked={this.handleCellClicked}
-        />
-      </div>
-    );
-  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          data-testid="app-link"
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Registration onNewGame={handleNewGame} />
+      <Game
+        player1={player1}
+        player2={player2}
+        board={board}
+        onCellClicked={handleCellClicked}
+      />
+    </div>
+  );
 }
 
 export default App;
